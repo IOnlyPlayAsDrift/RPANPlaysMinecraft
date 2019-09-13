79ea39a5f3e9d217d20773a89492c8dd8153d464
IOnlyPlayAsDrift <49349404+IOnlyPlayAsDrift@users.noreply.github.com>
2019-09-13 00:15:15 -0700

    Update RPAN Plays Minecraft.py

--- RPAN Plays Minecraft.py
+++ RPAN Plays Minecraft.py
@@ -1,60 +1,78 @@
 import praw
 import pyautogui
 import time
+from idlecolors import *
 
 reddit = praw.Reddit(client_id='placeholder',
                      client_secret='placeholder',
                      user_agent='placeholder')
 
-submissions = reddit.subreddit('pan_media')
+subreddit = reddit.subreddit('pan_media')
 
-for comment in submissions.stream.comments():
-    print(30*'_')
-    print()
-    try:
-        print(comment.author)
-        comment = comment.body
-        print(comment)
-        if comment == "openinventory": pyautogui.press('e');
-        if comment == "closeinventory": pyautogui.press('e');
-        if comment == "moveforward": pyautogui.keyDown('w');
-        if comment == "moveforwardcancel": pyautogui.press(['w', 'w', 'w']);
-        if comment == "movebackward": pyautogui.keyDown('s');
-        if comment == "movebackwardcancel": pyautogui.pyautogui.press(['s', 's', 's']);
-        if comment == "moveleft": pyautogui.keyDown('a');
-        if comment == "moveleftcancel": pyautogui.press(['a', 'a', 'a']);
-        if comment == "moveright": pyautogui.keyDown('d');
-        if comment == "moverightcancel": pyautogui.press(['d', 'd', 'd']);
-        if comment == "slot1": pyautogui.press('1');
-        if comment == "slot2": pyautogui.press('2');
-        if comment == "slot3": pyautogui.press('3');
-        if comment == "slot4": pyautogui.press('4');
-        if comment == "slot5": pyautogui.press('5');
-        if comment == "slot6": pyautogui.press('6');
-        if comment == "slot7": pyautogui.press('7');
-        if comment == "slot8": pyautogui.press('8');
-        if comment == "slot9": pyautogui.press('9');
-        if comment == "attackmob": pyautogui.mouseDown(duration=10.0);
-        if comment == "attackcancel": pyautogui.click(button='primary');
-        if comment == "mineveryshort": pyautogui.mouseDown(duration=1.0);
-        if comment == "mineshort": pyautogui.mouseDown(duration=3.0);
-        if comment == "minenormal": pyautogui.mouseDown(duration=5.0);
-        if comment == "minelong": pyautogui.mouseDown(duration=7.0);
-        if comment == "mineverylong": pyautogui.mouseDown(duration=9.0);
-        if comment == "minecancel": pyautogui.click(button='primary');
-        if comment == "lookleft": pyautogui.keyDown('num4'); time.sleep(1); pyautogui.keyUp('num4');
-        if comment == "lookright": pyautogui.keyDown('num6'); time.sleep(1); pyautogui.keyUp('num6');
-        if comment == "lookup": pyautogui.keyDown('num8'); time.sleep(1); pyautogui.keyUp('num8');
-        if comment == "lookdown": pyautogui.keyDown('num2'); time.sleep(1); pyautogui.keyUp('num2');
-        if comment == "resetcamera": pyautogui.keyDown('num5'); pyautogui.keyUp('num5');
-        if comment == "jumpup": pyautogui.keyDown('space'); pyautogui.keyUp('space');
-        if comment == "leftclick": pyautogui.click(button='primary');
-        if comment == "rightclick": pyautogui.click(button='right');
-        if comment == "eatfood": pyautogui.mouseDown(button='right', duration=5.0);
-        if comment == "menuup": pyautogui.press('num8');
-        if comment == "menudown": pyautogui.press('num2');
-        if comment == "menuleft": pyautogui.press('num4');
-        if comment == "menuright": pyautogui.press('num6');
+for comment in subreddit.stream.comments():
+    if comment.parent() == "placeholder":
+        print(30*'_')
+        print()
+        try:
+            print(comment.author)
+            comment = comment.body
+            printc( randcol(comment))
+            if comment == "openinv": pyautogui.press('e');
+            if comment == "closeinv": pyautogui.press('e');
+            if comment == "invopen": pyautogui.press('e');
+            if comment == "invclose": pyautogui.press('e');
+            if comment == "forward": pyautogui.keyDown('w');
+            if comment == "forwardstop": pyautogui.press(['w', 'w', 'w']);
+            if comment == "fwd": pyautogui.keyDown('w');
+            if comment == "fwdstop": pyautogui.press(['w', 'w', 'w']);
+            if comment == "backward": pyautogui.keyDown('s');
+            if comment == "backwardstop": pyautogui.press(['s', 's', 's']);
+            if comment == "bkwd": pyautogui.keyDown('s');
+            if comment == "bkwdstop": pyautogui.press(['s', 's', 's']);
+            if comment == "left": pyautogui.keyDown('a');
+            if comment == "leftstop": pyautogui.press(['a', 'a', 'a']);
+            if comment == "right": pyautogui.keyDown('d');
+            if comment == "rightstop": pyautogui.press(['d', 'd', 'd']);
+            if comment == "1": pyautogui.press('1');
+            if comment == "2": pyautogui.press('2');
+            if comment == "3": pyautogui.press('3');
+            if comment == "4": pyautogui.press('4');
+            if comment == "5": pyautogui.press('5');
+            if comment == "6": pyautogui.press('6');
+            if comment == "7": pyautogui.press('7');
+            if comment == "8": pyautogui.press('8');
+            if comment == "9": pyautogui.press('9');
+            if comment == "attack": pyautogui.click(clicks=10, interval=0.25);
+            if comment == "attackstop": pyautogui.click(button='primary');
+            if comment == "atk": pyautogui.click(clicks=10, interval=0.25);
+            if comment == "atkstop": pyautogui.click(button='primary');
+            if comment == "minevs": pyautogui.mouseDown(duration=1.0);
+            if comment == "mines": pyautogui.mouseDown(duration=3.0);
+            if comment == "mine": pyautogui.mouseDown(duration=5.0);
+            if comment == "minel": pyautogui.mouseDown(duration=7.0);
+            if comment == "minevl": pyautogui.mouseDown(duration=9.0);
+            if comment == "minestop": pyautogui.click(button='primary');
+            if comment == "cleft": pyautogui.keyDown('num4'); time.sleep(0.5); pyautogui.keyUp('num4');
+            if comment == "cright": pyautogui.keyDown('num6'); time.sleep(0.5); pyautogui.keyUp('num6');
+            if comment == "cup": pyautogui.keyDown('num8'); time.sleep(0.5); pyautogui.keyUp('num8');
+            if comment == "cdown": pyautogui.keyDown('num2'); time.sleep(0.5); pyautogui.keyUp('num2');
+            if comment == "creset": pyautogui.keyDown('x'); pyautogui.keyUp('x');
+            if comment == "camleft": pyautogui.keyDown('num4'); time.sleep(0.5); pyautogui.keyUp('num4');
+            if comment == "camright": pyautogui.keyDown('num6'); time.sleep(0.5); pyautogui.keyUp('num6');
+            if comment == "camup": pyautogui.keyDown('num8'); time.sleep(0.5); pyautogui.keyUp('num8');
+            if comment == "camdown": pyautogui.keyDown('num2'); time.sleep(0.5); pyautogui.keyUp('num2');
+            if comment == "camreset": pyautogui.keyDown('x'); pyautogui.keyUp('x');
+            if comment == "jump": pyautogui.keyDown('space'); pyautogui.keyUp('space');
+            if comment == "lc": pyautogui.click(button='primary');
+            if comment == "rc": pyautogui.click(button='right');
+            if comment == "eat": pyautogui.mouseDown(button='right');
+            if comment == "eatstop": pyautogui.click(button='right');
+            if comment == "menuup": pyautogui.press('num8');
+            if comment == "menudown": pyautogui.press('num2');
+            if comment == "menuleft": pyautogui.press('num4');
+            if comment == "menuright": pyautogui.press('num6');
+            if comment == "select": pyautogui.press('enter');
+            if comment == "place": pyautogui.press('enter');
 
-    except:
-        pass
+        except:
+            pass
